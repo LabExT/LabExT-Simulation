556f9f4baa454a2b36391b0987a7968f710a043c
Malte Wächter <git@malte-waechter.com>
2022-03-30 13:52:17 +0200

    Add setup.py pyproject.toml and .gitignore

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name="LabExT_Simulation",
+    version="0.0.1",
+    author="Malte Wächter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maltewae/LabExT_Simulation",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'LabExT-Simulation = src.Main:main',
+        ],
+    },
+)
